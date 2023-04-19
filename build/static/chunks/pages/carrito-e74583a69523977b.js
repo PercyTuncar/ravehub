e0d6d42ed4dd20674a1ced528bb79032b10f4d14
Percy Tuncar <33427262+PercyTuncar@users.noreply.github.com>
2023-04-19 01:26:25 -0500

    solution in component react

--- build/static/chunks/pages/carrito-e74583a69523977b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[993],{3211:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/carrito",function(){return u(8430)}])},8430:function(){}},function(n){n.O(0,[774,888,179],function(){return n(n.s=3211)}),_N_E=n.O()}]);
---
+++ pages/carrito.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CarritoPage = () => {
+  return <div>Tu carrito de compras está vacío.</div>;
+};
+
+export default CarritoPage;
